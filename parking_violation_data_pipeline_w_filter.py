07b85e22cabd0765afd7931b78a13f4e27b43d17
JosephMachado <joseph.machado.professional@gmail.com>
2021-05-21 11:44:48 -0400

    SDE-20210521: idempotent data pipelines

---
+++ parking_violation_data_pipeline_w_filter.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+
+import pandas as pd
+from typing import Any, List
+
+
+def run_parking_violations_data_pipeline(
+    input_file: str, output_loc: str, run_id: str
+) -> None:
+
+    df = pd.read_csv(input_file)
+    # your transformations
+    states_to_remove = ["99"]
+    df_fin = df[~df["Registration State"].isin(states_to_remove)]
+    df_fin.to_parquet(
+        os.path.join(output_loc, run_id), partition_cols=["Registration State"]
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--input-file",
+        type=str,
+        help="The input file",
+    )
+    parser.add_argument(
+        "--output-loc",
+        type=str,
+        help="The output folder",
+    )
+    parser.add_argument(
+        "--run-id",
+        type=str,
+        help="The day of run, in yyyymmdd format",
+    )
+
+    opts = parser.parse_args()
+    run_parking_violations_data_pipeline(
+        input_file=opts.input_file, output_loc=opts.output_loc, run_id=opts.run_id
+    )
